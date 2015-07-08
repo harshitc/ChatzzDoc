96e82859ffcf7be074019f57c0a7ae5b2a6ecbae
prateek276 <prateekmishra276@gmail.com>
2015-07-08 23:19:20 +0530

    some changes

---
+++ ChatApp/PrateekPDM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp
+{
+   public class PrateekPDM
+    {
+       public string id { get; set; }
+       public string fname { get; set; }
+       public string lname { get; set; }
+       public string uname { get; set; }
+       public string password { get; set; }
+       public string dob { get; set; }
+       public string gender { get; set; }
+       public string country { get; set; }
+       public string phone { get; set; }
+    }
+}
